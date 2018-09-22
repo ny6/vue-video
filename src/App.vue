d228aeeb626282f6761e4347c4b3045d1ac276c2
Yeshu Kochher <yeshu@outlook.in>
2018-09-22 11:15:46 +0530

    one where add eslint and consuming youtube api for search

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
-    Hello Vue!
+    <SearchBar @termChange="onTermChange" />
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import SearchBar from './components/SearchBar.vue';
+
+const { VUE_APP_API_KEY: key } = process.env;
+const url = 'https://www.googleapis.com/youtube/v3/search';
+
 export default {
   name: 'App',
+  components: { SearchBar },
+  methods: {
+    onTermChange(q) {
+      axios.get(url, {
+        params: {
+          key, type: 'video', part: 'snippet', q,
+        },
+      }).then(({ data }) => {
+        console.log(data);
+      });
+    },
+  },
 };
 </script>
